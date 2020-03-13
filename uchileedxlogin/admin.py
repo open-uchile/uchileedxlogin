e5cd3ff627ad4fd5fc9db2aa9ee992ddff60439c
Luchin101 <luis.santana101@gmail.com>
2020-03-13 11:00:09 -0300

    Admin Modified

--- uchileedxlogin/admin.py
+++ uchileedxlogin/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import EdxLoginUser, EdxLoginUserCourseRegistration
 
 # Register your models here.
-admin.site.register(EdxLoginUser)
-admin.site.register(EdxLoginUserCourseRegistration)
+
+class EdxLoginUserAdmin(admin.ModelAdmin):
+    list_display = ('run', 'user')
+    search_fields = ['run', 'user']
+    ordering = ['-run']
+
+class EdxLoginUserCourseRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('run', 'course')
+    search_fields = ['run', 'course']
+    ordering = ['-course']
+
+admin.site.register(EdxLoginUser, EdxLoginUserAdmin)
+admin.site.register(EdxLoginUserCourseRegistration, EdxLoginUserCourseRegistrationAdmin)
--- uchileedxlogin/settings/common.py
+++ uchileedxlogin/settings/common.py
@@ -1,5 +1,5 @@
 def plugin_settings(settings):
-    edxlogin_host = 'http://172.25.14.92'
+    edxlogin_host = 'http://172.25.14.97'
 
     settings.EDXLOGIN_RESULT_VALIDATE = edxlogin_host + ':9513/validate'
     settings.EDXLOGIN_USER_INFO_URL = edxlogin_host + ':8181/cxf/por-atributos-usuario/usuario'
